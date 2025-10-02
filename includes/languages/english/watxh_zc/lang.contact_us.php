66055fc4d334b41abf7558d28bdb5a9a2c18f3c6
Kyi <khantkhant528@gmail.com>
2025-10-03 00:52:41 +0700

    fixed logs

---
+++ includes/languages/english/watxh_zc/lang.contact_us.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('HEADING_CONTACT_US_POPUP_TITLE')) {
+    define('HEADING_CONTACT_US_POPUP_TITLE', 'Contact Us');
+    define('HEADING_CONTACT_US_POPUP_SUBTITLE', 'We\'d love to hear from you');
+    define('HEADING_CONTACT_US_FULL_NAME', 'Full Name');
+    define('HEADING_CONTACT_US_EMAIL', 'Email Address');
+    define('HEADING_CONTACT_US_MESSAGE', 'Your Message');
+    define('HEADING_CONTACT_US_SEND_MESSAGE', 'Send Message');
+}
--- includes/templates/watxh_zc/templates/tpl_modules_contact_us_popup.php
+++ includes/templates/watxh_zc/templates/tpl_modules_contact_us_popup.php
@@ -1,17 +1,5 @@
 <?php
-// Try to load language file
-@require_once(DIR_WS_LANGUAGES . $_SESSION['language'] . '/lang.contact_us.php');
-
-// If constants are not defined, define them manually as fallback
-if (!defined('HEADING_CONTACT_US_POPUP_TITLE')) {
-    define('HEADING_CONTACT_US_POPUP_TITLE', 'Contact Us');
-    define('HEADING_CONTACT_US_POPUP_SUBTITLE', 'We\'d love to hear from you');
-    define('HEADING_CONTACT_US_FULL_NAME', 'Full Name');
-    define('HEADING_CONTACT_US_EMAIL', 'Email Address');
-    define('HEADING_CONTACT_US_MESSAGE', 'Your Message');
-    define('HEADING_CONTACT_US_SEND_MESSAGE', 'Send Message');
-}
-
+@require_once(DIR_WS_LANGUAGES . $_SESSION['language'] . '/watxh_zc/lang.contact_us.php');
 // Initialize variables
 $name = '';
 $email_address = '';
